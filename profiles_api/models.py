32f959f4a756b3faf391d0ed664d211dded1c2eb
Begnazar Akhmadjonov <begnazar.akhmadjonov@ericsson.com>
2024-07-08 21:38:29 +0200

    Commit UserProfile Migrations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin, BaseUserManager
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+
+    def create_user(self, name, email, password=None):
+        if not email:
+            raise Exception("Email should be provided")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_super_user(self, email, name, password):
+
+        user = self.create_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.TextField(max_length=200)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name"]
+
+    def get_fullname(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name
+
+    def __str__(self) -> str:
+        return self.email
