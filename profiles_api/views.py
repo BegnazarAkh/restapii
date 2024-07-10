e6d3a49fda29de9c8eeab5215815d4602220aee0
Begnazar Akhmadjonov <begnazar.akhmadjonov@ericsson.com>
2024-07-10 15:44:08 +0200

    Filters and Permission

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,10 +2,17 @@ from rest_framework.views import APIView
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
-from profiles_api import serializers, models
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+
+from profiles_api import serializers, models, permissions
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
 
     serializer_class = serializers.ProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_field = ("name", "email")
